e3b439c9e666e118c27471d7df902009f6f4b41d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-02-16 22:09:24 +0000

    Move compress to utilities Continuing merge of DAE changes
    Former-commit-id: 2338be6982ae661654de9a666780e7260540618c

---
+++ utilitiesApp/src/compress.cpp
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include "zlib.h"
+
+#include <boost/scoped_array.hpp>
+
+#include <epicsExport.h>
+
+#include "utilities.h"
+
+// compress a string and then convert compressed bytes into an ascii hex sequence
+// suitable for transmission via an epics char waveform
+epicsShareFunc int epicsShareAPI compressString(const std::string& str, std::string& comp_str)
+{
+        uLong len = str.size() + 1; // include terminating NULL
+        uLong comprLen = compressBound(len);
+        boost::scoped_array<Byte> compr(new Byte[comprLen]);
+		comp_str.resize(0);
+        int err = compress(compr.get(), &comprLen, (const Bytef*)str.c_str(), len);
+		if (err != Z_OK)
+		{
+			std::cerr << "compressString: compress failed with error " << err << std::endl;
+			return -1;
+		}
+        std::ostringstream oss;
+		oss << std::hex << std::setfill('0') << std::setw(2);
+        for(int i=0; i<comprLen; ++i)
+        {
+            oss << static_cast<unsigned>(compr[i]);
+        }
+        comp_str = oss.str();
+		return 0;
+}
+
+epicsShareFunc int epicsShareAPI uncompressString(const std::string& comp_str, std::string& str)
+{
+	int length = str.length();
+	str.resize(0);
+	if (length % 2 != 0) // strings created by compressString() are always an even number of bytes
+	{
+		std::cerr << "uncompressString: input length not an even number of characters" << std::endl;
+		return -1;
+	}
+	std::vector<Byte> compr(length / 2);
+	for(int i=0; i < length; i+=2)
+	{
+		compr.push_back(static_cast<Byte>(strtol(comp_str.substr(i,2).c_str(), NULL, 16)));
+	}
+	uLong uncomprLen = std::max(length * 10, 65536);
+	boost::scoped_array<Byte> uncompr(new Byte[uncomprLen]);
+	int err = uncompress(uncompr.get(), &uncomprLen, &(compr[0]), compr.size()); 
+	if (err != Z_OK)
+	{
+		std::cerr << "uncompressString: uncompress failed with error " << err << std::endl;
+		return -1;
+	}
+	str.reserve(uncomprLen);
+	for(int i=0; (i < uncomprLen) && (uncompr[i] != '\0'); ++i)
+	{
+		str.push_back(static_cast<char>(uncompr[i]));
+	}
+    return 0;
+}
--- utilitiesApp/src/utilities.h
+++ utilitiesApp/src/utilities.h
@@ -5,6 +5,7 @@ epicsShareFunc std::string epicsShareAPI setIOCName(const char* iocName);
 epicsShareFunc std::string epicsShareAPI getIOCName();
 epicsShareFunc std::string epicsShareAPI getIOCGroup();
 epicsShareFunc std::string epicsShareAPI trimString(const std::string& str);
-
+epicsShareFunc int epicsShareAPI compressString(const std::string& str, std::string& comp_str);
+epicsShareFunc int epicsShareAPI uncompressString(const std::string& comp_str, std::string& str);
 
 #endif /* UTILITIES_H */
