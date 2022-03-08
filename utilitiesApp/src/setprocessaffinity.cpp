ddffd9c07a42d3589a08e9c4beac56296f1a642c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-03-08 10:51:31 +0000

    Formatting

--- utilitiesApp/src/setprocessaffinity.cpp
+++ utilitiesApp/src/setprocessaffinity.cpp
@@ -10,17 +10,17 @@ int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-	    std::cerr << "Usage: file mask" << std::endl;
-		return 1;
+        std::cerr << "Usage: file mask" << std::endl;
+        return 1;
     }
     const char* file = argv[1];
     DWORD mask = atoll(argv[2]);
-	LOADED_IMAGE li;
-	if (MapAndLoad(file, "\\nosearch", &li, 0, 0) == 0)
-	{
-	    std::cerr << "MapAndLoad: error " << GetLastError() << std::endl;
-		return 1;
-	}
+    LOADED_IMAGE li;
+    if (MapAndLoad(file, "\\nosearch", &li, 0, 0) == 0)
+    {
+        std::cerr << "MapAndLoad: error " << GetLastError() << std::endl;
+        return 1;
+    }
     ULONG lcds;
     PIMAGE_LOAD_CONFIG_DIRECTORY plcd = (PIMAGE_LOAD_CONFIG_DIRECTORY)ImageDirectoryEntryToDataEx(
         li.MappedAddress,
@@ -32,20 +32,20 @@ int main(int argc, char* argv[])
     if (!plcd)
     {
       std::cerr << "ImageDirectoryEntryToDataEx: error " << GetLastError() << std::endl;
-	  return 1;
+      return 1;
     }
     if (lcds != plcd->Size)
     {
       std::cerr << "ImageDirectoryEntryToDataEx: size error " << lcds << "!=" << plcd->Size << std::endl;
-	  return 1;
+      return 1;
     }
-	std::cout << "Old ProcessAffinityMask: " << plcd->ProcessAffinityMask << std::endl;
+    std::cout << "Old ProcessAffinityMask: " << plcd->ProcessAffinityMask << std::endl;
     plcd->ProcessAffinityMask = mask;
-	std::cout << "New ProcessAffinityMask: " << plcd->ProcessAffinityMask << std::endl;
-	if (UnMapAndLoad(&li) == 0)
-	{
-	    std::cerr << "UnMapAndLoad: error " << GetLastError() << std::endl;
-		return 1;
-	}
-	return 0;
+    std::cout << "New ProcessAffinityMask: " << plcd->ProcessAffinityMask << std::endl;
+    if (UnMapAndLoad(&li) == 0)
+    {
+        std::cerr << "UnMapAndLoad: error " << GetLastError() << std::endl;
+        return 1;
+    }
+    return 0;
 }
